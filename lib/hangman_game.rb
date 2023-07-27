daa5be9634c0d9e6bb726914eeb1bf7fb4b697df
Adrian Perez <adrian1804perez@gmail.com>
2023-07-27 09:48:18 -0700

    Add random word method

--- lib/hangman_game.rb
+++ lib/hangman_game.rb
@@ -1,1 +1,9 @@
-words = File.open()
+def random_word
+  words = File.readlines('alot_of_words.txt')
+  possible_words = []
+  words.each do |word|
+    word = word.chomp
+    possible_words.push(word) if word.length >= 5 && word.length <= 12 # Criteria
+  end
+  possible_words.sample # This will return one word from the list of possible words
+end
